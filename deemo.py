3754eb8fd980aefc7d1d6282c2900e4bfd1c8036
Jinyoung <jiynoung3635@gmail.com>
2018-05-25 22:36:34 +0900

    add deemo.py

---
+++ deemo.py
@@ -0,0 +1,17 @@
+import asyncio
+import discord
+
+client = discord.Client()
+
+token = "NDQ5NDI0MzE2NzE5ODI0ODk3.DekfVA.SBdHjXbsSVySV5hzBPwYYfSq9Ak"
+
+@client.event
+async def on_ready():
+    print("Logged in as")
+    print(client.user.name)
+    print(client.user.id)
+    print("=======")
+
+    await client.change_presence(game=discord.Game(name="CodeAligo :d",type=1))
+
+client.run(token)
