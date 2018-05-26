120dae13b72db9ec5ddb2585b2a3d30bee0a1e0e
Jinyoung <jiynoung3635@gmail.com>
2018-05-26 16:25:49 +0900

    메세지기능 추가

--- deemo.py
+++ deemo.py
@@ -14,4 +14,18 @@ async def on_ready():
 
     await client.change_presence(game=discord.Game(name="CodeAligo :D",type=1))
 
+@client.event
+async def on_message(message):
+    if message.authon.bot:
+        return None
+
+id = message.author.id
+channel = message.channel 
+
+if message.content.startswith('!커맨드'):
+    aait clien.send_message(channel, '커맨드')
+else:
+
+    await client.send_message(channel, "<@"+id+">님이 \""+message.content+"\"라고 말하였습니다.")
+
 client.run(token)
