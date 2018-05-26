5e3be0a4cb7a7a92602b18784654035ee0de5086
Jinyoung <jiynoung3635@gmail.com>
2018-05-26 16:35:15 +0900

    새로운 기능 피드백

--- deemo.py
+++ deemo.py
@@ -14,18 +14,20 @@ async def on_ready():
 
     await client.change_presence(game=discord.Game(name="CodeAligo :D",type=1))
 
+
+# 봇이 새로운 메시지를 수신했을때 동작되는 코드입니다.
 @client.event
 async def on_message(message):
-    if message.authon.bot:
-        return None
+    if message.author.bot: #만약 메시지를 보낸사람이 봇일 경우에는
+        return None #동작하지 않고 무시합니다.
 
-id = message.author.id
-channel = message.channel 
+    id = message.author.id #id라는 변수에는 메시지를 보낸사람의 ID를 담습니다.
+    channel = message.channel #channel이라는 변수에는 메시지를 받은 채널의 ID를 담습니다.
 
-if message.content.startswith('!커맨드'):
-    aait clien.send_message(channel, '커맨드')
-else:
-
-    await client.send_message(channel, "<@"+id+">님이 \""+message.content+"\"라고 말하였습니다.")
+    if message.content.startswith('!커맨드'): #만약 해당 메시지가 '!커맨드' 로 시작하는 경우에는
+        await client.send_message(channel, '커맨드') #봇은 해당 채널에 '커맨드' 라고 말합니다.
+    else: #위의 if에 해당되지 않는 경우
+        #메시지를 보낸사람을 호출하며 말한 메시지 내용을 그대로 출력해줍니다.
+        await client.send_message(channel, "<@"+id+">님이 \""+message.content+"\"라고 말하였습니다.")
 
 client.run(token)
